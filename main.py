71c5ca25e478e5bf1964b6fb88e47ccdcc259c72
Harish <harish.rajkumar2022@vitstudent.ac.in>
2024-06-05 23:29:53 +0530

    Add add-task route

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import json
+from models import Task
 
 app = FastAPI()
 
@@ -6,3 +8,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/tasks/")
+async def add_task(task: Task):
+    with open("db.json", "r") as f:
+        tasks = json.load(f)
+    
+    tasks["tasks"].append(task.text)
+
+    with open("db.json", "w") as f:
+        json.dump(tasks, f, indent=4)

cda452eaf3688887965dbe96c153f783f1222922
Harish <harish.rajkumar2022@vitstudent.ac.in>
2024-06-06 13:02:22 +0530

    Fix tasks tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -2,7 +2,7 @@ from fastapi.testclient import TestClient
 from ..main import app
 import pytest
 import json
-from ..schemas.tasks import AddTaskModel
+from ..schemas.tasks import TaskModel
 
 @pytest.fixture
 def client():
@@ -18,15 +18,16 @@ def test_get_tasks(client):
     assert response.json()["tasks"] == tasks
 
 def test_add_task(client):
-    new_task = AddTaskModel(text="Test task")
+    new_task = TaskModel(text="Test task")
     
     response = client.post("/tasks/", json=new_task.model_dump())
     assert response.status_code == 200
     assert response.json() == new_task.model_dump()
 
     with open("db.json") as f:
-        tasks = json.load(f)["tasks"]
+        db = json.load(f)
+        tasks = db["tasks"]
         tasks.remove(new_task.text)
     
     with open("db.json", "w") as f:
-        json.dump({"tasks": tasks}, f, indent=4)
+        json.dump(db, f, indent=4)

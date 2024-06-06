67fd05cab9ab1cf710ca9dc859cbbefcefe9e645
Harish <harish.rajkumar2022@vitstudent.ac.in>
2024-06-06 09:29:28 +0530

    Set up helper function to interact with gemini

---
+++ utils/ai.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+URL = f'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={os.getenv("GEMINI_API_KEY")}'
+
+
+def get_ai_response(prompt):
+    headers = {'Content-Type': 'application/json'}
+    data = {"contents":[{"parts":[{"text":str(prompt)}]}]}
+    response = requests.post(URL, headers=headers, data=json.dumps(data))
+    
+    return response.json()["candidates"][0]["content"]["parts"][0]["text"]
+
+
+if __name__ == '__main__':
+    print(get_ai_response('Hello, how are you?'))
